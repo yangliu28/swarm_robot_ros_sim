2ddb216566e0efcbea8d29b14647637678db3d2f
yangliu28 <yxl1450@case.edu>
2015-11-09 17:48:54 -0500

    fix some parameters in two_wheel_robot.urdf

--- swarm_robot_controller/src/two_wheel_pos_minimal_controller.cpp
+++ swarm_robot_controller/src/two_wheel_pos_minimal_controller.cpp
@@ -25,20 +25,22 @@ double dt = 0.01; // sample time for the controller
 // used in the apply joint effort service message as duration
 
 // global variable
-double g_kp = 10.0;  // to be tuned
-double g_kv = 3.0;  // to be tuned
-VectorXd g_left_wheel_poses;
-VectorXd g_left_wheel_vels;
-VectorXd g_left_wheel_poses_cmd;  // no need to specify velocity for cmd
-VectorXd g_right_wheel_poses;
-VectorXd g_right_wheel_vels;
-VectorXd g_right_wheel_poses_cmd;
-// std::vector<double> g_left_wheel_poses;
-// std::vector<double> g_left_wheel_vels;
-// std::vector<double> g_left_wheel_poses_cmd;  // no need to specify velocity in command
-// std::vector<double> g_right_wheel_poses;
-// std::vector<double> g_right_wheel_vels;
-// std::vector<double> g_right_wheel_poses_cmd;
+// double g_kp = 0.1;  // to be tuned
+// double g_kv = 0.03;  // to be tuned
+double g_kp = 0.0;  // to be tuned
+double g_kv = 0.0;  // to be tuned
+// VectorXd g_left_wheel_poses;
+// VectorXd g_left_wheel_vels;
+// VectorXd g_left_wheel_poses_cmd;  // no need to specify velocity for cmd
+// VectorXd g_right_wheel_poses;
+// VectorXd g_right_wheel_vels;
+// VectorXd g_right_wheel_poses_cmd;
+std::vector<double> g_left_wheel_poses;
+std::vector<double> g_left_wheel_vels;
+std::vector<double> g_left_wheel_poses_cmd;  // no need to specify velocity in command
+std::vector<double> g_right_wheel_poses;
+std::vector<double> g_right_wheel_vels;
+std::vector<double> g_right_wheel_poses_cmd;
 bool g_cmd_callback_started = false;  // cmd callback has been invoked at least once
 
 // int to string converter
@@ -51,15 +53,15 @@ std::string intToString(int a) {
 // callback for message from topic "two_wheel_poses"
 void twoWheelPosesCallback(const swarm_robot_msgs::two_wheel_poses& message_holder) {
     // convert from array to Eigen type
-    swarm_robot_msgs::two_wheel_poses pos_msg = message_holder;  // make a copy
-    g_left_wheel_poses = MapVectorXd(&pos_msg.left_wheel_pos[0], pos_msg.left_wheel_pos.size());
-    g_left_wheel_vels = MapVectorXd(&pos_msg.left_wheel_vel[0], pos_msg.left_wheel_vel.size());
-    g_right_wheel_poses = MapVectorXd(&pos_msg.right_wheel_pos[0], pos_msg.right_wheel_pos.size());
-    g_right_wheel_vels = MapVectorXd(&pos_msg.right_wheel_vel[0], pos_msg.right_wheel_vel.size());
-    // g_left_wheel_poses = pos_msg.left_wheel_pos;
-    // g_left_wheel_vels = pos_msg.left_wheel_vel;
-    // g_right_wheel_poses = pos_msg.right_wheel_pos;
-    // g_right_wheel_vels = pos_msg.right_wheel_vel;
+    // swarm_robot_msgs::two_wheel_poses pos_msg = message_holder;  // make a copy
+    // g_left_wheel_poses = MapVectorXd(&pos_msg.left_wheel_pos[0], pos_msg.left_wheel_pos.size());
+    // g_left_wheel_vels = MapVectorXd(&pos_msg.left_wheel_vel[0], pos_msg.left_wheel_vel.size());
+    // g_right_wheel_poses = MapVectorXd(&pos_msg.right_wheel_pos[0], pos_msg.right_wheel_pos.size());
+    // g_right_wheel_vels = MapVectorXd(&pos_msg.right_wheel_vel[0], pos_msg.right_wheel_vel.size());
+    g_left_wheel_poses = message_holder.left_wheel_pos;
+    g_left_wheel_vels = message_holder.left_wheel_vel;
+    g_right_wheel_poses = message_holder.right_wheel_pos;
+    g_right_wheel_vels = message_holder.right_wheel_vel;
     // check if cmd callback started yet
     if (!g_cmd_callback_started) {
         // let the cmd poses be the same as current poses
@@ -73,11 +75,11 @@ void twoWheelPosesCmdCallback(const swarm_robot_msgs::two_wheel_poses& message_h
     if (!g_cmd_callback_started)  // first time to be invoked
         g_cmd_callback_started = true;
     // convert from array to Eigen type
-    swarm_robot_msgs::two_wheel_poses pos_msg = message_holder;  // make a copy
-    g_left_wheel_poses_cmd = MapVectorXd(&pos_msg.left_wheel_pos[0], pos_msg.left_wheel_pos.size());
-    g_right_wheel_poses_cmd = MapVectorXd(&pos_msg.right_wheel_pos[0], pos_msg.right_wheel_pos.size());
-    // g_left_wheel_poses_cmd = pos_msg.left_wheel_pos;
-    // g_right_wheel_poses_cmd = pos_msg.right_wheel_pos;
+    // swarm_robot_msgs::two_wheel_poses pos_msg = message_holder;  // make a copy
+    // g_left_wheel_poses_cmd = MapVectorXd(&pos_msg.left_wheel_pos[0], pos_msg.left_wheel_pos.size());
+    // g_right_wheel_poses_cmd = MapVectorXd(&pos_msg.right_wheel_pos[0], pos_msg.right_wheel_pos.size());
+    g_left_wheel_poses_cmd = message_holder.left_wheel_pos;
+    g_right_wheel_poses_cmd = message_holder.right_wheel_pos;
 }
 
 // callback function for the service "two_wheel_kpkv"
@@ -141,33 +143,47 @@ int main(int argc, char **argv) {
 
     // prepare control variables
     ros::Rate rate_timer(1/dt);
-    VectorXd left_wheel_poses_err;
-    VectorXd right_wheel_poses_err;
-    VectorXd left_wheel_torque;
-    VectorXd right_wheel_torque;
+    // VectorXd left_wheel_poses_err;
+    // VectorXd right_wheel_poses_err;
+    // VectorXd left_wheel_torque;
+    // VectorXd right_wheel_torque;
     // std::vector<double> left_wheel_poses_err;
     // std::vector<double> right_wheel_poses_err;
     // std::vector<double> left_wheel_torque;
     // std::vector<double> right_wheel_torque;
-    left_wheel_poses_err.resize(robot_quantity);
-    right_wheel_poses_err.resize(robot_quantity);
-    left_wheel_torque.resize(robot_quantity);
-    right_wheel_torque.resize(robot_quantity);
+    // left_wheel_poses_err.resize(robot_quantity);
+    // right_wheel_poses_err.resize(robot_quantity);
+    // left_wheel_torque.resize(robot_quantity);
+    // right_wheel_torque.resize(robot_quantity);
+    double left_wheel_pos_err;
+    double left_wheel_torque;
+    double right_wheel_pos_err;
+    double right_wheel_torque;
     std::string left_motor_name;  // name of left motor
     std::string right_motor_name;  // name of right motor
     // control loop
     while (ros::ok()) {
-        // calculate the error
-        left_wheel_poses_err = g_left_wheel_poses_cmd - g_left_wheel_poses;
-        right_wheel_poses_err = g_right_wheel_poses_cmd - g_right_wheel_poses;
-        // control algorithm, calculate the torque to be exerted
-        left_wheel_torque = g_kp * left_wheel_poses_err - g_kv * g_left_wheel_vels;
-        right_wheel_torque = g_kp * right_wheel_poses_err - g_kv * g_right_wheel_vels;
+        ROS_INFO("in the loop...");
+        // // calculate the error
+        // left_wheel_poses_err = g_left_wheel_poses_cmd - g_left_wheel_poses;
+        // right_wheel_poses_err = g_right_wheel_poses_cmd - g_right_wheel_poses;
+        // // control algorithm, calculate the torque to be exerted
+        // left_wheel_torque = g_kp * left_wheel_poses_err - g_kv * g_left_wheel_vels;
+        // right_wheel_torque = g_kp * right_wheel_poses_err - g_kv * g_right_wheel_vels;
 
         // apply joint effort
         for (int i=0; i<robot_quantity; i++) {
-            // do not watch for periodicity, that is, make error in (-M_PI, M_PI)
+            // calculate the error
+            left_wheel_pos_err = g_left_wheel_poses_cmd[i] - g_left_wheel_poses[i];
+            right_wheel_pos_err = g_right_wheel_poses_cmd[i] - g_right_wheel_poses[i];
+
+            // do not watch for periodicity, that is, making error in (-M_PI, M_PI)
             // because wheels are continuous rotating, no need to be in that range
+
+            // calculate the torque
+            left_wheel_torque = g_kp * left_wheel_pos_err - g_kv * g_left_wheel_vels[i];
+            right_wheel_torque = g_kp * right_wheel_pos_err - g_kv * g_right_wheel_vels[i];
+            left_wheel_torque = 0.00001;
 
             std::string s_index = intToString(i);
             // actually cheat here, no better way to get the key words "left_motor" and "right_motor"
@@ -176,12 +192,13 @@ int main(int argc, char **argv) {
 
             // set left wheel torque
             set_wheel_torque_srv_msg.request.joint_name = left_motor_name;
-            set_wheel_torque_srv_msg.request.effort = left_wheel_torque[i];
+            set_wheel_torque_srv_msg.request.effort = left_wheel_torque;
+            ROS_INFO_STREAM(left_wheel_torque);
             set_wheel_torque_client.call(set_wheel_torque_srv_msg);
 
             // set right wheel torque
             set_wheel_torque_srv_msg.request.joint_name = right_motor_name;
-            set_wheel_torque_srv_msg.request.effort = right_wheel_torque[i];
+            set_wheel_torque_srv_msg.request.effort = right_wheel_torque;
             set_wheel_torque_client.call(set_wheel_torque_srv_msg);
         }
 
