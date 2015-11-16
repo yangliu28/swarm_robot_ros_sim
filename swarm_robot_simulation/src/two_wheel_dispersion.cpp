9ca8f2eb33fb2da6cdc026505c418f61519e0f81
yangliu28 <yxl1450@case.edu>
2015-11-15 23:47:02 -0500

    finish the two wheel dispersion implementation

--- swarm_robot_simulation/src/two_wheel_dispersion.cpp
+++ swarm_robot_simulation/src/two_wheel_dispersion.cpp
@@ -4,6 +4,8 @@
 // subscribe to topic "swarm_robot_poses" to get current robot poses
 // create an action client to "two_wheel_traj_action"
 
+// impact avoiding is not implemented here, what's the better way to do that?
+
 #include <ros/ros.h>
 #include <math.h>
 #include <swarm_robot_msgs/swarm_robot_poses.h>
@@ -19,9 +21,13 @@ bool g_robot_poses_cb_started = false;
 // simulation control parameters
 const double spring_length = 0.5;  // spring length when not compressed or extended
 const double upper_limit_ratio = 0.50;  // upper limit part relative to spring length
+const double upper_limit = spring_length * (1 + upper_limit_ratio);
 const double feedback_ratio = 0.382;  // smaller than 1 to make it stable, golden ratio ;)
 
-const double upper_limit = spring_length * (1 + upper_limit_ratio);
+// two wheel robot specification, really should get these values in another way
+const double half_wheel_dist = 0.0177;
+const double wheel_radius = 0.015;
+const double wheel_speed = 0.5;  // rad*s-1, for the calculation of time cost of the action
 
 // callback for message from topic "swarm_robot_poses"
 void swarmRobotPosesCb(const swarm_robot_msgs::swarm_robot_poses& message_holder) {
@@ -73,25 +79,118 @@ int main(int argc, char **argv) {
 
     // all the above are copied from example_wheel_traj_interpolator_ac
 
+    // variables for the loop
     double distance[robot_quantity][robot_quantity];  // two dimensional array to store distance
     double distance_sort[robot_quantity][robot_quantity];
     int index_sort[robot_quantity][robot_quantity];
+    // initialize index_sort
+    for (int i=0; i<robot_quantity; i++) {
+        for (int j=0; j<robot_quantity; j++) {
+            index_sort[i][j] = j;
+        }
+    }
+    // the loop of optimizing robot position for dispersion
     while (ros::ok()) {
         ros::spinOnce();  // update robot positions
 
         // calculate the distance between robots
         for (int i=0; i<robot_quantity; i++) {
             for (int j=i; j<robot_quantity; j++) {
-                distance[i][j] = sqrt(pow(g_robot_x[i] - g_robot_x[j], 2), 
+                distance[i][j] = sqrt(pow(g_robot_x[i] - g_robot_x[j], 2) +
                     pow(g_robot_y[i] - g_robot_y[j], 2));
                 if (i != j)  // not in diagonal axis
                     distance[j][i] = distance[i][j];  // symmetrical matrix
             }
         }
 
-        // sort these distance
-        distance_sort = distance;
-    }
+        // sort the distances and get sorted index
+        for (int i=0; i<robot_quantity; i++)
+            for (int j=0; j<robot_quantity; j++)
+                distance_sort[i][j] = distance[i][j];  // initialize distance_sort
+        double distance_temp;
+        int index_temp;
+        for (int i=0; i<robot_quantity; i++) {
+            for (int j=0; j<robot_quantity-1; j++) {
+                for (int k=0; k<robot_quantity-1-j; k++) {
+                    if (distance_sort[i][k] > distance_sort[i][k+1]) {
+                        // switch value between distance_sort[i][k] and distance_sort[i][k+1] 
+                        distance_temp = distance_sort[i][k];
+                        distance_sort[i][k] = distance_sort[i][k+1];
+                        distance_sort[i][k+1] = distance_temp;
+                        // switch value between index_sort[i][k] and index[i][k+1]
+                        index_temp = index_sort[i][k];
+                        index_sort[i][k] = index_sort[i][k+1];
+                        index_sort[i][k+1] = index_temp;
+                    }
+                }
+            }
+        }
 
+        // check whether the closest is itself
+        for (int i=0; i<robot_quantity; i++) {
+            if (index_sort[i][0] != i) {
+                ROS_WARN("error in the sorting of robot distance");
+                return 0;
+            }
+        }
+
+        // find all the neighbors that will be used in the force feedback control
+        // the algorithm used here is the same with the algorithm implemented in matlab before
+        // no matter how far away one robot is with others, 3 closest neighbors will be counted
+        // no matter how close one robot is with others, 6 closest neighbors at most will be counted
+        // which means there should be at least 6 robots
+        int neighbor_num[robot_quantity];  // the number of valid neighbors
+        for (int i=0; i<robot_quantity; i++) {
+            neighbor_num[i] = 3;  // there are at least 3 neighbors
+            while (distance_sort[i][neighbor_num[i]+1] < upper_limit && neighbor_num[i] <= 6)
+                neighbor_num[i] = neighbor_num[i] + 1;
+        }
+
+        // calculate displacement of each robot
+        double displacement_x[robot_quantity];  // displacement in x
+        double displacement_y[robot_quantity];  // displacement in y
+        double distance_diff;
+        for (int i=0; i<robot_quantity; i++) {
+            displacement_x[i] = 0.0;
+            displacement_y[i] = 0.0;
+            double distance_diff_sum = 0.0;
+            for (int j=1; j<=neighbor_num[i]; j++) {
+                distance_diff = distance_sort[i][j] - spring_length;
+                // add the spring effect of all the neighbors
+                displacement_x[i] = displacement_x[i] + feedback_ratio * distance_diff * 
+                    (g_robot_x[index_sort[i][j]] - g_robot_x[i]) / distance_sort[i][j];
+                displacement_y[i] = displacement_y[i] + feedback_ratio * distance_diff * 
+                    (g_robot_y[index_sort[i][j]] - g_robot_y[i]) / distance_sort[i][j];
+            }
+        }
+
+        // prepare the goal message
+        double displacement_average = 0.0;  // for the calculation of time cost
+        for (int i=0; i<robot_quantity; i++) {
+            goal.x[i] = g_robot_x[i] + displacement_x[i];
+            goal.y[i] = g_robot_y[i] + displacement_y[i];
+            displacement_average = displacement_average +
+                sqrt(pow(displacement_x[i], 2) + pow(displacement_y[i], 2));
+        }
+        // this is the real average displacement
+        displacement_average = displacement_average / robot_quantity;
+        // use displacement_average to represent time spent on line movement
+        // use M_PI/3 to represent time spent on self rotating
+        goal.time_cost = displacement_average / wheel_radius / wheel_speed + 
+            M_PI/3 * half_wheel_dist / wheel_radius / wheel_speed;
+
+        // send out goal
+        action_client.sendGoal(goal);
+        // wait for expected duration plus some tolerance (2 seconds)
+        bool finish_before_timeout = action_client.waitForResult(ros::Duration(goal.time_cost + 2.0));
+        if (!finish_before_timeout) {
+            ROS_WARN("this action is not done...timeout");
+            return 0;
+        }
+        else {
+            ROS_INFO("this action is done.");
+        }
+    }
+    return 0;
 }
 
