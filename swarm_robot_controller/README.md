99571d098071e27ada13595ee62744e4600f1ea7
yangliu28 <yxl1450@case.edu>
2016-06-21 12:32:24 -0400

    update velocity controller node

--- swarm_robot_controller/README.md
+++ swarm_robot_controller/README.md
@@ -1,33 +1,3 @@
 # swarm_robot_controller
 
-Cpp files:
 
-    swarm_robot_pos_publisher.cpp: publish messages on topic "swarm_robot_poses"
-
-    two_wheel_pos_publisher.cpp: publish messages on topic "two_wheel_poses"
-
-    two_wheel_pos_minimal_controller.cpp
-
-    two_wheel_kpkv_service_client.cpp
-
-    example_wheel_pos_cmd_publisher.cpp (for test)
-
-    two_wheel_traj_interpolator_as.cpp
-
-    example_wheel_traj_interpolator_ac.cpp (for test)
-
-Launch file:
-
-    two_wheel_robot_minimal_controller.launch
-
-        swarm_robot_pos_publisher
-
-        two_wheel_pos_publisher
-
-        two_wheel_pos_minimal_controller
-
-
-## Example usage
-
-## Running tests/demos
-    
--- swarm_robot_controller/src/two_wheel_robot_wheel_vel_controller.cpp
+++ swarm_robot_controller/src/two_wheel_robot_wheel_vel_controller.cpp
@@ -12,18 +12,31 @@
 #include <swarm_robot_msg/two_wheel_robot_wheel_vel_cmd.h>
 #include <gazebo_msgs/ApplyJointEffort.h>
 
+const double TOPIC_ACTIVE_PERIOD = 1.0;  // threshold to judge if topics are active
+const double CONTROL_PERIOD = 0.001;
+
 // global variables
 swarm_robot_msg::two_wheel_robot current_robots;
-
+swarm_robot_msg::two_wheel_robot_wheel_vel_cmd current_vel_cmd;
+// time stamps for callbacks, used to check topic activity
+ros::Time two_wheel_robot_topic_timer, vel_cmd_topic_timer;
+bool two_wheel_robot_topic_active, vel_cmd_topic_active;
 
 // the callback for getting two wheel robot information
-void twoWheelRobotCallback(const swarm_robot_msg::two_wheel_robot& current_two_wheel_robot) {
-    // 
+    // the update-to-date robot status will be decided only by this topic
+    // because the publisher on velocity command is also rely on this topic
+void twoWheelRobotCallback(const swarm_robot_msg::two_wheel_robot& input_two_wheel_robot) {
+    // update the time stamp when this callback is invoked the last time
+    two_wheel_robot_topic_timer = ros::Time::now();
+    current_robots = input_two_wheel_robot;  // update msg in global variable
 }
 
 // the callback for receiving wheel velocity command
-void twoWheelRobotVelCmdCallback(const swarm_robot_msg::two_wheel_robot_wheel_vel_cmd& current_vel_cmd) {
-    // 
+void twoWheelRobotVelCmdCallback(const swarm_robot_msg::two_wheel_robot_wheel_vel_cmd&
+    input_vel_cmd) {
+    // update the time stamp when this callback is invoked the last time
+    vel_cmd_topic_timer = ros::Time::now();
+    current_vel_cmd = input_vel_cmd;  // update msg in global variable
 }
 
 int main(int argc, char **argv) {
@@ -43,18 +56,80 @@ int main(int argc, char **argv) {
         return 0;
     }
 
+    // check if gazebo service apply_joint_effort is ready
+    ros::Duration half_sec(0.5);
+    bool service_ready = ros::service::exists("/gazebo/apply_joint_effort", true);
+    if (!service_ready) {
+        // service not ready
+        while (!service_ready) {
+            ROS_INFO("waiting for gazebo service apply_joint_effort");
+            half_sec.sleep();
+            service_ready = ros::service::exists("/gazebo/apply_joint_effort", true);
+        }
+    }
+    ROS_INFO("gazebo service apply_joint_effort is ready");
+
     // instantiate a subscriber for "/swarm_sim/two_wheel_robot"
     ros::Subscriber two_wheel_robot_subscriber
         = nh.subscribe("/swarm_sim/two_wheel_robot", 1, twoWheelRobotCallback);
+
+    // instantiate a subscriber for "/swarm_sim/two_wheel_robot_wheel_vel_cmd"
+    ros::Subscriber two_wheel_robot_wheel_vel_cmd_subscriber
+        = nh.subscribe("/swarm_sim/two_wheel_robot_wheel_vel_cmd", 1, twoWheelRobotVelCmdCallback);
 
     // instantiate a service client for "/gazebo/apply_joint_effort"
     ros::ServiceClient apply_joint_effort_client
         = nh.serviceClient<gazebo_msgs::ApplyJointEffort>("/gazebo/apply_joint_effort");
     gazebo_msgs::ApplyJointEffort apply_joint_effort_srv_msg;
 
-    // instantiate a subscriber for "/swarm_sim/two_wheel_robot_wheel_vel_cmd"
-    ros::Subscriber two_wheel_robot_wheel_vel_cmd_subscriber
-        = nh.subscribe("/swarm_sim/two_wheel_robot_wheel_vel_cmd", 1, twoWheelRobotVelCmdCallback);
+    // initialize callback timer
+    two_wheel_robot_topic_timer = ros::Time::now();
+    vel_cmd_topic_timer = ros::Time::now();
+    ros::Duration(TOPIC_ACTIVE_PERIOD + 0.1).sleep();  // avoid false judgement of active
+
+    // control wheel velocity loop
+    ros::Time timer_now;
+    ros::Rate loop_rate(1/CONTROL_PERIOD);
+    while (ros::ok()) {
+        // check if two wheel robot and wheel vel cmd topics are active
+        timer_now = ros::Time::now();
+        if ((timer_now - two_wheel_robot_topic_timer).toSec() < TOPIC_ACTIVE_PERIOD)
+            two_wheel_robot_topic_active = true;
+        else
+            two_wheel_robot_topic_active = false;
+        if ((timer_now - vel_cmd_topic_timer).toSec() < TOPIC_ACTIVE_PERIOD)
+            vel_cmd_topic_active = true;
+        else
+            vel_cmd_topic_active = false;
+
+        // 
+        if (two_wheel_robot_topic_active) {
+            // preapre to control wheel velocity only when this topic is ready
+            int robot_quantity = current_robots.index.size();
+            swarm_robot_msg::two_wheel_robot_wheel_vel_cmd processed_vel_cmd;
+            processed_vel_cmd.index = current_robots.index;
+            processed_vel_cmd.left_wheel_vel_cmd.resize(robot_quantity);
+            processed_vel_cmd.right_wheel_vel_cmd.resize(robot_quantity);
+            // prepare the processed_vel_cmd from current_vel_cmd
+            if (vel_cmd_topic_active) {
+                // the velocity command topic is active
+                
+            }
+            for (int i=0; i<robot_quantity; i++) {
+
+            }
+        }
+
+
+
+
+
+
+        loop_rate.sleep();
+        ros::spinOnce();  // let the global variables update
+    }
+
+
 
 
 
