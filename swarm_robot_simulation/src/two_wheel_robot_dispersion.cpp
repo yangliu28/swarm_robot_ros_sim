281cb4010b89070041214ba4921e46831f8a8f40
yangliu28 <yxl1450@case.edu>
2016-06-21 19:56:44 -0400

    update the package control

--- swarm_robot_simulation/src/two_wheel_robot_dispersion.cpp
+++ swarm_robot_simulation/src/two_wheel_robot_dispersion.cpp
@@ -9,13 +9,40 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "two_wheel_robot_dispersion");
     ros::NodeHandle nh;
 
+    // handshake with robot name in parameter server
+    std::string robot_name;
+    bool get_name;
+    get_name = nh.getParam("/swarm_sim/robot_name", robot_name);
+    if (!get_name) {
+        ROS_ERROR("simulation environment(parameters) is not set");
+        return 0;
+    }
+    if (robot_name != "two_wheel_robot") {
+        ROS_ERROR("wrong robot according to parameter server");
+        return 0;
+    }
 
+    // check if gazebo service set_joint_properties is ready
+    ros::Duration half_sec(0.5);
+    bool service_ready = ros::service::exists("/gazebo/set_joint_properties", true);
+    if (!service_ready) {
+        // service not ready
+        while (!service_ready) {
+            ROS_INFO("waiting for gazebo service set_joint_properties");
+            half_sec.sleep();
+            service_ready = ros::service::exists("/gazebo/set_joint_properties", true);
+        }
+    }
+    ROS_INFO("gazebo service set_joint_properties is ready");
 
+    // instantiate a subscriber to topic "/swarm_sim/two_wheel_robot"
+    ros::Subscriber two_wheel_robot_subscriber
+        = nh.subscribe("/swarm_sim/two_wheel_robot", 1, twoWheelRobotCallback);
 
-
-
-
-
+    // instantiate a service client for "/gazebo/set_joint_properties"
+    ros::ServiceClient set_joint_properties_client
+        = nh.serviceClient<gazebo_msgs::SetJointProperties>("/gazebo/set_joint_properties");
+    gazebo_msgs::SetJointProperties set_joint_properties_srv_msg;
 
 
 
