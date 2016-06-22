ccf026afb6e3c48b50d6091c573e8ec878e4e96a
yangliu28 <yxl1450@case.edu>
2016-06-21 23:26:46 -0400

    process robot position data

--- swarm_robot_simulation/src/two_wheel_robot_dispersion.cpp
+++ swarm_robot_simulation/src/two_wheel_robot_dispersion.cpp
@@ -3,9 +3,13 @@
 #include <ros/ros.h>
 #include <swarm_robot_msg/two_wheel_robot.h>
 #include <gazebo_msgs/SetJointProperties.h>
+#include <math.h>
 
 const double TOPIC_ACTIVE_PERIOD = 1.0;  // threshold to tell if a topic is active
 const double CONTROL_PERIOD = 0.001;
+
+const int NEIGHBOR_NUM_L_LIMIT = 3;
+const int NEIGHBOR_NUM_H_LIMIT = 6;
 
 // global variables
 swarm_robot_msg::two_wheel_robot current_robots;
@@ -21,7 +25,7 @@ void twoWheelRobotCallback(const swarm_robot_msg::two_wheel_robot& input_msg) {
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "two_wheel_robot_dispersion");
-    ros::NodeHandle nh;
+    ros::NodeHandle nh("~");
 
     // handshake with robot name in parameter server
     std::string robot_name;
@@ -49,6 +53,20 @@ int main(int argc, char **argv) {
     }
     ROS_INFO("gazebo service set_joint_properties is ready");
 
+    // get settings for this simulation from private parameter
+    double spring_length = 0.7;  // default spring length
+    bool get_spring_length = nh.getParam("spring_length", spring_length);
+    if (get_spring_length) {
+        ROS_INFO_STREAM("using spring length passed in: " << spring_length);
+        // delete parameter
+        nh.deleteParam("spring_length");
+    }
+    else
+        ROS_INFO_STREAM("using default spring length: 0.7");
+    // calculate other parameter depending on spring length
+    double upper_limit_ratio = 0.30;
+    double upper_limit = spring_length * (1 + upper_limit_ratio);
+
     // instantiate a subscriber to topic "/swarm_sim/two_wheel_robot"
     ros::Subscriber two_wheel_robot_subscriber
         = nh.subscribe("/swarm_sim/two_wheel_robot", 1, twoWheelRobotCallback);
@@ -74,14 +92,94 @@ int main(int argc, char **argv) {
         timer_now = ros::Time::now();
         if ((timer_now - two_wheel_robot_topic_timer).toSec() < TOPIC_ACTIVE_PERIOD) {
             // the topic is been actively published
-            // set the stop_once_all_robot flag, prepare when topic out of active state
+            // set the stop_once_all_robots flag, prepare when topic out of active state
             stop_once_all_robots = true;
-            //
+
+            // 1.calculate distance between any two robots
+            int robot_quantity = current_robots.index.size();
+            double distance[robot_quantity][robot_quantity];
+            for (int i=0; i<robot_quantity; i++) {
+                for (int j=i; j<robot_quantity; j++) {
+                    if (i == j) {
+                        // zero for the diagonal
+                        distance[i][j] = 0;
+                    }
+                    else {
+                        distance[i][j] = sqrt(pow(current_robots.x[i] - current_robots.x[j], 2)
+                            + pow(current_robots.y[i] - current_robots.y[j], 2));
+                        // symmetrical matrix, copy the other side
+                        distance[j][i] = distance[i][j];
+                    }
+                }
+            }
+
+            // 2.sort the distance and record the index change
+            double distance_sort[robot_quantity][robot_quantity];
+            int index_sort[robot_quantity][robot_quantity];
+            // initialize distance_sort and index_sort
+            for (int i=0; i<robot_quantity; i++) {
+                for (int j=0; j<robot_quantity; j++) {
+                    distance_sort[i][j] = distance[i][j];
+                    index_sort[i][j] = j;
+                }
+            }
+            // start sorting, bubble sort method
+            double distance_temp;
+            int index_temp;
+            for (int i=0; i<robot_quantity; i++) {
+                for (int j=0; j<robot_quantity-1; j++) {
+                    for (int k=0; k<robot_quantity-1-j; k++) {
+                        if (distance_sort[i][k] > distance_sort[i][k+1]) {
+                            // switch between these two distances
+                            distance_temp = distance_sort[i][k];
+                            distance_sort[i][k] = distance_sort[i][k+1];
+                            distance_sort[i][k+1] = distance_temp;
+                            // also switch corresponding indices
+                            index_temp = index_sort[i][k];
+                            index_sort[i][k] = index_sort[i][k+1];
+                            index_sort[i][k+1] = index_temp;
+                        }
+                    }
+                }
+            }
+
+            // 3.find all neighbors that will be used in force feedback control
+            // find all the neighbors within the upper limit, choose closest 6 if exceed 6
+            // if neighbors are sparse, override upper limit and choose cloest 3
+            // i.e., there are 3 ~ 6 neighbors for each robot
+            int neighbor_num[robot_quantity];  // the number of vallid neighbors
+            // no need to record index
+            // they are the first neighbor_num robots in the sorted list
+            for (int i=0; i<robot_quantity; i++) {
+                // compare robot quantity with neighbor number limits
+                if ((robot_quantity - 1) <= NEIGHBOR_NUM_L_LIMIT) {
+                    // (robot_quantity - 1) is the largest neighbor number for each robot
+                    // set neighbor number to the largest possible
+                    neighbor_num[i] = robot_quantity - 1;
+                }
+                else {
+                    neighbor_num[i] = NEIGHBOR_NUM_L_LIMIT;  // initialize with lower limit
+                    for (int j=NEIGHBOR_NUM_L_LIMIT+1; j<robot_quantity; j++) {
+                        // (NEIGHBOR_NUM_L_LIMIT+1) is the index in the distance_sort
+                        // of the first robot except itself and the neighbors
+                        if (distance_sort[i][j] < upper_limit) {
+                            neighbor_num[i] = neighbor_num[i] + 1;
+                            if (neighbor_num[i] == NEIGHBOR_NUM_H_LIMIT)
+                                break;
+                        }
+                    }
+                }
+            }
+
+            // 4.
         }
         else {
             // the topic is not active
-            if (!stop_once_all_robots) {
+            if (stop_once_all_robots) {
                 // set wheel speed of all robots to zero according to last topic update
+                
+                // reset the stop_once_all_robots flag
+                stop_once_all_robots = false;
             }
         }
 
